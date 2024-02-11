6cb10bece56ae970cf221c5e728c0b2f7e668c31
NicPWNs <nic@nicpjones.com>
2024-02-11 12:19:08 -0500

    feat: 200 Response

--- lambda_function.py
+++ lambda_function.py
@@ -9,9 +9,6 @@ from nacl.exceptions import BadSignatureError
 
 # Load Secrets
 load_dotenv()
-TABLE = getenv("DDB_TABLE")
-GITHUB = getenv("GITHUB_CLIENT")
-DISCORD = getenv("DISCORD_TOKEN")
 PUBLIC_KEY = getenv("PUBLIC_KEY")
 
 
@@ -40,30 +37,14 @@ def lambda_handler(event, context):
     if body["type"] == 1:
         return {"type": 1}
 
-    # Application Command Context
-    channel = body["channel_id"]
-    interaction = body["data"]["id"]
-    token = body["token"]
-
-    # Get Options
-    repository = body["data"]["options"][0]["value"]
-    events = body["data"]["options"][1]["name"]
-
-    # Extract Repo and Owner
-    repo_search = search(r"[\/\/]*[github\.com]*[\/]*([\w.-]+)\/([\w.-]+)", repository)
-
-    if repo_search:
-        owner = repo_search.group(1)
-        repo = repo_search.group(2)
-
+    # 200 Response
     embeds = [
         {
-            "type": "rich",
             "title": "GitHub",
-            "description": f"<#{channel}> Subscribing to {events}\nat [`{owner}/{repo}`](https://github.com/{owner}/{repo})",
-            "color": 0xFFFFFF,
+            "description": "⏳  Loading...",
+            "color": 0xFEE9B6,
             "thumbnail": {
-                "url": "https://github.githubassets.com/images/modules/open_graph/github-logo.png"
+                "url": "https://raw.githubusercontent.com/StylishThemes/GitHub-Dark/master/images/octocat-spinner-128-bright.gif"
             },
         }
     ]
