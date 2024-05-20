1c7ffc633f76385b0e33662b10f2f098c87caa17
NicPWNs <nic@nicpjones.com>
2024-05-20 19:45:32 -0400

    chore: remove print()

--- lambda_processor.py
+++ lambda_processor.py
@@ -268,7 +268,6 @@ def lambda_processor(event, context):
         pass
 
     # Interaction Context
-    print(event)
     repository = event["data"]["options"][0]["options"][0]["value"]
     events = event["data"]["options"][0]["options"][1]["value"]
     discord_user_id = event["member"]["user"]["id"]
